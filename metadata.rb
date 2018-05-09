3af6a6f508b476ff138d97e4652795222577cfeb
Tony Fruzza <anthony.fruzza@sturdynetworks.com>
2018-05-09 10:47:01 -0700

    sync up supermarket with latest

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license 'MIT'
 description 'Installs/Configures slack_speak'
 long_description 'Installs/Configures slack_speak'
 source_url 'https://github.com/tonyfruzza/slack_speak'
-version '0.1.7'
+version '0.1.8'
 
 depends 'aws', '~> 7.3'
